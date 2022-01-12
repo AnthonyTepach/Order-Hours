324081228844f2f2f83c9f2ad8150567b4818494
AnthonyTch <anthony.tepach@gmail.com>
2022-01-12 08:58:25 -0600

    leer linea completa de regritro

--- src/main/java/com/anthnytepach/app/Main.java
+++ src/main/java/com/anthnytepach/app/Main.java
@@ -1,18 +1,17 @@
 package com.anthnytepach.app;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String line="16:27:34,15:34:09,06:49:14";
-        String array_time[]=line.split(",");
+        String line="542\tMaria Francisca\tTorres Velazquez\t\tFemenino\t21\tQUINCENA\t1\tCargo\t11/01/2022\t3\t15:30:55,16:25:29,07:50:43\n";
+        String array_time[]=line.split("\t");
 
         List<String> times=Arrays.asList(array_time);
-
         Collections.sort(times);
+
 
         for (String a:times){
             System.out.println(a.substring(0,a.length()-3));
