8a1f9923c0527f7b74fc9f4fc8411a443beddb5b
AnthonyTch <anthony.tepach@gmail.com>
2022-01-12 10:13:35 -0600

    Creacion del metodo que nos permite recuperar la ruta de un archivo por su extencion.

--- src/main/java/com/anthnytepach/app/Files/FileBrowser.java
+++ src/main/java/com/anthnytepach/app/Files/FileBrowser.java
@@ -1,6 +1,10 @@
 package com.anthnytepach.app.Files;
 
-import javax.swing.*;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+import java.util.Map;
 
 public class FileBrowser extends JFileChooser {
 
@@ -9,5 +13,14 @@ public class FileBrowser extends JFileChooser {
         int result = super.showSaveDialog(null);
         if (result==JFileChooser.CANCEL_OPTION);
         return super.getSelectedFile().toString();
+    }
+
+    public String openFile(Map<String,String> typeFile){
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(typeFile.get("Desc"), typeFile.get("Ext"),typeFile.get("Type"));
+        super.setFileFilter(filter);
+        int result = super.showOpenDialog(null);
+        File file = super.getSelectedFile();
+        if (result==JFileChooser.CANCEL_OPTION);
+        return file.getAbsolutePath();
     }
 }
