f3be85d4acb6b83b45db958259f334d41dc8524f
AnthonyTch <anthony.tepach@gmail.com>
2022-01-12 09:55:59 -0600

    imprimir linea con formato correcto

--- src/main/java/com/anthnytepach/app/Main.java
+++ src/main/java/com/anthnytepach/app/Main.java
@@ -6,17 +6,21 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String line="542\tMaria Francisca\tTorres Velazquez\t\tFemenino\t21\tQUINCENA\t1\tCargo\t10/01/2022\t4\t16:59:02,07:37:35,16:01:39,18:54:42";
-        String array_items[]=line.split("\t");
-        String array_times[]=array_items[11].split(",");
+        String line = "542\tMaria Francisca\tTorres Velazquez\t\tFemenino\t21\tQUINCENA\t1\tCargo\t10/01/2022\t4\t16:59:02,07:37:35,16:01:39,18:54:42";
+        String array_items[] = line.split("\t");
+        String array_times[] = array_items[11].split(",");
 
-        List<String> times=Arrays.asList(array_times);
+        List<String> times = Arrays.asList(array_times);
         Collections.sort(times);
 
-
-        for (String a:times){
-            System.out.println(a.substring(0,a.length()-3));
+        String format_hours = "";
+        for (String a : times) {
+            format_hours = format_hours + "," + a.substring(0, a.length() - 3);
         }
-
+        String format_line = "";
+        for (int i = 0; i < array_items.length-1; i++) {
+            format_line=format_line+"\t"+array_items[i];
+        }
+        System.out.println(format_line.substring(1)+"\t"+format_hours.substring(1));
     }
 }
