bebd2c248ce90cf192ec4c90f71a7f5c2c683bd4
AnthonyTch <anthony.tepach@gmail.com>
2022-01-12 10:10:38 -0600

    Creacion del mettodo que noes permite seleccinar la ruta de guardado, extendemos de JFileChooser

--- src/main/java/com/anthnytepach/app/Files/FileBrowser.java
+++ src/main/java/com/anthnytepach/app/Files/FileBrowser.java
@@ -1,4 +1,13 @@
 package com.anthnytepach.app.Files;
 
-public class FileBrowser {
+import javax.swing.*;
+
+public class FileBrowser extends JFileChooser {
+
+    public String saveAs(){
+        super.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int result = super.showSaveDialog(null);
+        if (result==JFileChooser.CANCEL_OPTION);
+        return super.getSelectedFile().toString();
+    }
 }
