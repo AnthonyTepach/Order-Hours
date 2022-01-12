a758de5f37fa67853520785016a30ff82efd1bff
AnthonyTch <anthony.tepach@gmail.com>
2022-01-12 10:20:50 -0600

    leer documento completo.

--- src/main/java/com/anthnytepach/app/Main.java
+++ src/main/java/com/anthnytepach/app/Main.java
@@ -1,10 +1,24 @@
 package com.anthnytepach.app;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import com.anthnytepach.app.Files.FileBrowser;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class Main {
+    Map<String, String> ext;
+
+    public Main() {
+
+        ext = new HashMap<>();
+        ext.put("Desc", "Archivo de Texto");
+        ext.put("Ext", ".txt");
+        ext.put("Type", "TXT");
+    }
+
     public static void main(String[] args) {
         String line = "542\tMaria Francisca\tTorres Velazquez\t\tFemenino\t21\tQUINCENA\t1\tCargo\t10/01/2022\t4\t16:59:02,07:37:35,16:01:39,18:54:42";
         String array_items[] = line.split("\t");
@@ -18,9 +32,18 @@ public class Main {
             format_hours = format_hours + "," + a.substring(0, a.length() - 3);
         }
         String format_line = "";
-        for (int i = 0; i < array_items.length-1; i++) {
-            format_line=format_line+"\t"+array_items[i];
+        for (int i = 0; i < array_items.length - 1; i++) {
+            format_line = format_line + "\t" + array_items[i];
         }
-        System.out.println(format_line.substring(1)+"\t"+format_hours.substring(1));
+        System.out.println(format_line.substring(1) + "\t" + format_hours.substring(1));
+    }
+
+    public void reformatDocument() {
+        try {
+            List<String> listTxt = Files.readAllLines(Paths.get(new FileBrowser().openFile(ext)), StandardCharsets.UTF_8);
+
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
     }
 }
